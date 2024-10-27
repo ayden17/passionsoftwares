017c8b67d14977ca9e77568b326d1a40d826301f
Ayden Victory Haoken <44537272+ayden17@users.noreply.github.com>
2024-10-27 19:12:48 +0700

    Add files via upload

---
+++ Apps in Action Images Ayden/Ayden The Rizzard Chatbot! It's Open Source so use it to rizz your crush!/src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "6aee203e-2fd5-46c0-a386-4ee70ddf3f50");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
